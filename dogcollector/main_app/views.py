e7f50be218c8069ce5cb268773e19531e7d1a488
nanmardel <martin_n1@denison.edu>
2022-04-27 17:54:45 -0500

    M:M

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from .models import Dog
-
+from django.shortcuts import render, redirect
+from .models import Dog, Toy
+from .forms import FeedingForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 from django.http import HttpResponse # like res.render in Express
       
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']    
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'    
+
 
 def home(request):
     return HttpResponse('<h1>Hello World</h1>')
@@ -17,4 +32,40 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
-    return render(request, 'dogs/detail.html', {'dog': dog})
+    feeding_form = FeedingForm()
+    toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+    return render(request, 'dogs/detail.html', {'dog': dog, 'feeding_form': feeding_form,
+    'toys': toys_dog_doesnt_have
+    })
+
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+        return redirect('detail', dog_id=dog_id)
+    
+    
+def assoc_toy(request,dog_id, toy_id):
+    Dog.objects.get(id=dog_id).toys.add(toy_id) 
+    return redirect('detail', dog_id=dog_id)   
+    
+class ToyList(ListView):
+    model = Toy    
+    
+class ToyDetail(DetailView):
+    model = Toy    
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+    
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'            
